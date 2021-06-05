99ca5692976517d32161ba31446a5c6583d98c19
teerapongt <903435+teerapongt@users.noreply.github.com>
2021-06-06 00:42:05 +0700

    chore: add Serilog

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -5,19 +5,28 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+//using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace API
 {
     public class Program
     {
-        public async static Task Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
 
             using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
+                
+                Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console()
+                    .CreateLogger();
 
                 try
                 {
@@ -27,22 +36,27 @@ namespace API
                     {
                         await context.Database.MigrateAsync();
                     }
+                    
+                    await host.RunAsync();
+                    return 0;
                 }
                 catch (Exception ex)
                 {
-                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    Log.Error(ex, "An error occurred while migrating or seeding the database.");
+                    Log.Fatal(ex, "Host terminated unexpectedly");
 
-                    logger.LogError(ex, "An error occurred while migrating or seeding the database.");
-
-                    throw;
+                    return 1;
+                }
+                finally
+                {
+                    Log.CloseAndFlush();
                 }
             }
-
-            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -15,6 +15,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Serilog;
 
 namespace API
 {
@@ -50,6 +51,8 @@ namespace API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ABCBank API v1"));
             }
+            
+            app.UseSerilogRequestLogging();
             
             app.UseRouting();
 
