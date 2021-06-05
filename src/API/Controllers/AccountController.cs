54964363668bd12099d421c1a8cffb759785cfcc
teerapongt <903435+teerapongt@users.noreply.github.com>
2021-06-05 20:01:03 +0700

    docs(account): add error status code

--- src/API/Controllers/AccountController.cs
+++ src/API/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using API.Application.Account.Commands.Create;
 using API.Application.Account.Commands.Deposit;
@@ -12,6 +13,8 @@ namespace API.Controllers
     {
         [HttpGet("{IBAN}", Name = "GetByIBAN")]
         [ProducesResponseType(typeof(GetByIBANDto), 200)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(500)]
         public async Task<ActionResult<GetByIBANDto>> GetByIBAN(string IBAN)
         {
             var dto = await Mediator.Send(new GetByIBANQuery {IBAN = IBAN});
@@ -20,6 +23,8 @@ namespace API.Controllers
 
         [HttpPost]
         [ProducesResponseType(typeof(CreateDto), 201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
         public async Task<ActionResult<CreateDto>> Create([FromForm] CreateCommand command)
         {
             var dto = await Mediator.Send(command);
@@ -28,6 +33,8 @@ namespace API.Controllers
 
         [HttpPut("[action]/{IBAN}")]
         [ProducesResponseType(typeof(DepositDto), 200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
         public async Task<ActionResult<DepositDto>> Deposit(string IBAN, [FromForm] decimal amount)
         {
             var dto = await Mediator.Send(new DepositCommand {IBAN = IBAN, Amount = amount});
@@ -36,6 +43,8 @@ namespace API.Controllers
 
         [HttpPut("[action]/{IBAN}")]
         [ProducesResponseType(typeof(TransferDto), 200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
         public async Task<ActionResult<TransferDto>> Transfer(string IBAN, [FromForm] string ToIBAN,
             [FromForm] decimal amount)
         {
